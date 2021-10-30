9ad1f4db10870801f96b3ccb3d5b246951e1a928
Emel614 <emelny614@gmail.com>
2021-10-30 16:56:11 +0200

    concort

--- src/test/java/com/concorthotel/tests/US_002.java
+++ src/test/java/com/concorthotel/tests/US_002.java
@@ -1,4 +1,15 @@
 package com.concorthotel.tests;
 
+import com.concorthotel.pages.MainPage;
+import com.concorthotel.utilities.ConfigReader;
+import com.concorthotel.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class US_002 {
+    @Test
+    public void mainPageLoginLinkTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        MainPage mainPage = new MainPage();
+        mainPage.loginLink.click();
+    }
 }
