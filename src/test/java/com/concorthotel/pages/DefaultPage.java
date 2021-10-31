43cd03c77221602e0c70aeb4d6d313de81218265
bhrtrk <bahar.dk@gmail.com>
2021-10-31 11:54:02 +0100

    update

--- src/test/java/com/concorthotel/pages/DefaultPage.java
+++ src/test/java/com/concorthotel/pages/DefaultPage.java
@@ -1,4 +1,17 @@
 package com.concorthotel.pages;
 
+import com.concorthotel.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class DefaultPage {
+    public DefaultPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//span[@class='username username-hide-on-mobile']")
+    public WebElement managerText;
+
+    @FindBy(xpath = "//span[@class='title']")
+    public WebElement systemManagementText;
 }
