e42972c9eea523ed6e06b4b2113d7b49f9802c60
adarici <88286697+adarici@users.noreply.github.com>
2021-11-03 20:21:02 +0100

    Update EditHotelRoomPage.java

--- src/test/java/com/concorthotel/pages/EditHotelRoomPage.java
+++ src/test/java/com/concorthotel/pages/EditHotelRoomPage.java
@@ -1,4 +1,26 @@
 package com.concorthotel.pages;
 
-public class EditHotelRoomPage {
+import com.concorthotel.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DefaultPage {
+    public DefaultPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//span[@class='username username-hide-on-mobile']")
+    public WebElement managerText;
+
+    @FindBy(xpath = "//span[@class='title']")
+    public WebElement systemManagementText;
+
+    @FindBy(xpath = "(//span[@class='title'])[3]")
+    public WebElement hotelManagementLink;
+
+    @FindBy(xpath = "(//i[@class='icon-calendar'])[3]")
+    public WebElement hotelRoomsTab;
+
+
 }
+
